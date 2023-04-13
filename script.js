8fac8b464b9f0c043e55757622352521be49d019
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-04-13 18:11:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // complete the given function
 
 function palindrome(str){
-
+	str = str.toLowerCase();
+	let newStr = '';
+	for (let i = 0; i < str.length; i++) {
+		if(str[i] >= 'a' && str[i] <= 'z'){
+			newStr += str[i];
+		}
+	}
+	let left = 0, right = str.length - 1;
+	while (left < right) {
+		if(newStr[left] != newStr[right]){
+			return false;
+		}
+		left6++;
+		right--;
+	}
+	return true;
 }
 module.exports = palindrome
